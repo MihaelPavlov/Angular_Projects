1fc111867cfc33eeb0aa3b938ae1fbef8ae77cf2
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2023-09-06 17:39:18 +0300

    add canActivate guard and active route links

--- investment-app/src/app/modules/app-routing.module.ts
+++ investment-app/src/app/modules/app-routing.module.ts
@@ -1,19 +1,32 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {InvestmentDetailsComponent} from "../components/portfolio/investment-details/investment-details.component";
-import {AppComponent} from "../app.component";
 import {InvestmentListComponent} from "../components/portfolio/investment-list/investment-list.component";
+import {
+  AddUpdateInvestmentComponent
+} from "../components/portfolio/add-update-investment/add-update-investment.component";
+import {RegisterComponent} from "../components/register/register.component";
+import {LoginComponent} from "../components/login/login.component";
+import {AuthGuardService} from "../guards/auth-guard.service";
 
-export const routes: Routes = [
-  {path: '/', component: InvestmentListComponent, },
+const routes: Routes = [
+  {path: '', component: InvestmentListComponent},
+  {path: 'create', canActivate: [AuthGuardService], component: AddUpdateInvestmentComponent},
+  {path: 'update/:id', component: AddUpdateInvestmentComponent},
   {path: 'investment-details/:id', component: InvestmentDetailsComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
 ]
 
 @NgModule({
 
-  imports:[RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule{
+export class AppRoutingModule {
 
 }
