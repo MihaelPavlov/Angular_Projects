d7f642b8f6fcf6c422b46d29444380ec53f3a73a
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2023-09-13 22:41:28 +0300

    filter for my investment in progress

--- investment-app/src/app/components/portfolio/investment-list/investment-list.component.ts
+++ investment-app/src/app/components/portfolio/investment-list/investment-list.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, OnInit, TemplateRef, ViewChild} from "@angular/core";
 import {IInvestment} from "../../../models/investment";
 import {InvestmentService} from "../../../services/investment.service";
 import {MatTableDataSource} from "@angular/material/table";
@@ -9,6 +9,7 @@ import {ToastService} from "../../../../lib/services/toast.service";
 import {ToastType} from "../../../models/toast";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort, Sort} from "@angular/material/sort";
+import {Form, FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: "investment-list",
@@ -19,8 +20,21 @@ export class InvestmentListComponent implements OnInit, AfterViewInit {
   user?: IUser | null
   dataSource!: MatTableDataSource<IInvestment>;
   displayedColumns: string[] = ['investmentName', 'symbol', 'quantity', 'purchasePrice', 'investmentType', 'actions'];
+  filterColumns: { id: number, value: string }[] = Object.keys(Fields).map(x => ({
+    id: Number(x),
+    value: this.getEnumTextByKey(Number(x))
+  })).filter(x => x.value !== "")
+  labels: string[] = []
+  areFilterShown: boolean = false;
+  selectedColumn!: string
+  filterText!: string
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild("#applyFilter") myFilter!: TemplateRef<any>
+
+  filterForm = new FormGroup({
+    "filters": new FormArray([])
+  });
 
   constructor(private investmentService: InvestmentService, private router: Router,
               private authService: AuthService, private toastService: ToastService) {
@@ -30,7 +44,7 @@ export class InvestmentListComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.investmentService.getInvestments(Number(this.user?.id)).subscribe(x=>{
+    this.investmentService.getInvestments(Number(this.user?.id)).subscribe(x => {
       this.investmentService.fetchInvestments(x);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
@@ -55,7 +69,7 @@ export class InvestmentListComponent implements OnInit, AfterViewInit {
     this.investmentService.delete(id).subscribe({
       next: response => {
         this.toastService.success({message: 'Successfully Deleted', type: ToastType.Success})
-        this.investmentService.getInvestments(Number(this.user?.id)).subscribe(x=>{
+        this.investmentService.getInvestments(Number(this.user?.id)).subscribe(x => {
           this.investmentService.fetchInvestments(x);
         });
       },
@@ -68,4 +82,57 @@ export class InvestmentListComponent implements OnInit, AfterViewInit {
   onEdit(id: number): void {
     this.router.navigate(['update', id]);
   }
+
+  showFilters() {
+    console.log(this.filterForm.controls.filters)
+    this.areFilterShown = !this.areFilterShown;
+    console.log('selected column ->', this.selectedColumn)
+    console.log('filtered text ->', this.filterText)
+  }
+
+  AddInput(column: { id:number, value: string}) {
+    console.log("add input", column);
+    this.labels.push(column.value);
+    console.log('labels', this.labels);
+    (<FormArray>this.filterForm.get('filters')).push(new FormControl(null, Validators.required));
+  }
+
+  removeInput(index: number) {
+    (<FormArray>this.filterForm.get('filters')).removeAt(index);
+    this.labels.splice(index, 1);
+  }
+
+  onSubmitFilters() {
+    console.log(this.getEnumValueByName('investmentName'))
+    console.log('filter form -=>', this.filterForm)
+  }
+
+  getEnumValueByName(enumName: string): number | undefined {
+    return Fields[enumName as keyof typeof Fields];
+  }
+
+  getEnumTextByKey(key: number): string {
+    switch (key) {
+      case Fields.investmentName:
+        return "Investment Name";
+      case Fields.symbol:
+        return "Symbol";
+      case Fields.quantity:
+        return "Quantity";
+      case Fields.purchasePrice:
+        return "Purchase Price";
+      case Fields.investmentType:
+        return "Investment Type";
+    }
+    return "";
+  }
 }
+
+export enum Fields {
+  investmentName = 0,
+  symbol = 1,
+  quantity = 2,
+  purchasePrice = 3,
+  investmentType = 4,
+}
+
