d7d87b6bcce1f7a36ab95fa2edbb57131d46a86f
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2023-09-21 09:36:02 +0300

    migration components , fix toast messages

--- investment-app/src/app/app.module.ts
+++ investment-app/src/app/app.module.ts
@@ -6,36 +6,27 @@ import {LoginComponent} from "./components/login/login.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RegisterComponent} from "./components/register/register.component";
 import {MatExpansionModule} from "@angular/material/expansion";
-import {MatLegacySelectModule as MatSelectModule} from '@angular/material/legacy-select';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
   AddUpdateInvestmentComponent
 } from "./components/portfolio/add-update-investment/add-update-investment.component";
 import {InvestmentListComponent} from "./components/portfolio/investment-list/investment-list.component";
-import {MatLegacyPaginatorModule as MatPaginatorModule} from "@angular/material/legacy-paginator";
-import {MatLegacyTableModule as MatTableModule} from "@angular/material/legacy-table";
 import {CurrencyToSymbolPipe} from "./pipes/currency-to-symbol.pipe";
 import {InvestmentTypeToNamePipe} from "./pipes/investment-type-to-name.pipe";
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
-import {MatLegacyMenuModule as MatMenuModule} from "@angular/material/legacy-menu";
 import {MatIconModule} from "@angular/material/icon";
 import {InvestmentDetailsComponent} from "./components/portfolio/investment-details/investment-details.component";
-import {MatLegacyCardModule as MatCardModule} from "@angular/material/legacy-card";
 import {HttpModule} from "../lib/http/http.module";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {ToolBarComponent} from "./components/toolbar/tool-bar.component";
 import {NewsListComponent} from "./components/news/news-list/news-list.component";
-import {MatLegacySnackBarModule as MatSnackBarModule} from "@angular/material/legacy-snack-bar";
 import {ToastComponent} from "./components/shared/toast/toast.component";
 import {TOAST_DEFAULT_OPTIONS} from "./models/toast";
 import {
   DetailsPopUpComponent
 } from "./components/news/details-pop-up/details-pop-up.component";
-import {MatLegacyDialogModule as MatDialogModule} from "@angular/material/legacy-dialog";
 import {
   DetailsCommentsPopUpComponent
 } from "./components/news/details-comments-pop-up/details-comments-pop-up.component";
-import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
 import {AppRoutingModule} from "./modules/app-routing.module";
 import {AuthGuardService} from "./guards/auth-guard.service";
 import {TestComponent} from "./components/test.component";
@@ -44,6 +35,18 @@ import {HomeComponent} from "./components/home/home.component";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./interceptors/auth.interceptor";
 import {MatSortModule} from "@angular/material/sort";
+import { EffectsModule } from '@ngrx/effects';
+import {StoreModule} from "@ngrx/store";
+import {MatSelectModule} from "@angular/material/select";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {CommonModule} from "@angular/common";
 
 // const routes: Routes = [
 //   {path: '', component: InvestmentListComponent},
@@ -74,6 +77,7 @@ import {MatSortModule} from "@angular/material/sort";
     HomeComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
@@ -94,7 +98,9 @@ import {MatSortModule} from "@angular/material/sort";
     MatDialogModule,
     MatInputModule,
     AppRoutingModule,
-    MatSortModule
+    MatSortModule,
+    StoreModule.forRoot({ }),
+    EffectsModule.forRoot([])
   ],
   providers: [
     TOAST_DEFAULT_OPTIONS,
--- investment-app/src/app/components/news/details-pop-up/details-pop-up.component.ts
+++ investment-app/src/app/components/news/details-pop-up/details-pop-up.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from "@angular/core";
-import {MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialog as MatDialog} from "@angular/material/legacy-dialog";
 import {INews} from "../../../models/news";
 import {DetailsCommentsPopUpComponent} from "../details-comments-pop-up/details-comments-pop-up.component";
+import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: "details-modal",
