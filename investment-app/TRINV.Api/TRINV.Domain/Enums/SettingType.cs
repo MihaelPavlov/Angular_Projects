5170f796803307586fad853e22bb0036f4ab7a71
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-22 20:20:48 +0200

    Added SettingType enum

---
+++ investment-app/TRINV.Api/TRINV.Domain/Enums/SettingType.cs
@@ -0,0 +1,14 @@
+﻿namespace TRINV.Domain.Enums
+{
+
+    public enum SettingType
+    {
+        Mail = 0,
+        Sms = 1,
+        Feedback = 2,
+        Emergency = 3,
+        Notification = 4,
+        Calculation = 5,
+        Animation = 6
+    }
+}
