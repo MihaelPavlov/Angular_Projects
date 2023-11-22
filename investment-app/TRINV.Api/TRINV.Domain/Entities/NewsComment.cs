7114da90822df43bdae67167dc9ce912aaec9425
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-22 21:36:25 +0200

    Added NewsComment Entity

---
+++ investment-app/TRINV.Api/TRINV.Domain/Entities/NewsComment.cs
@@ -0,0 +1,31 @@
+﻿namespace TRINV.Domain.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class NewsComment
+    {
+        public NewsComment()
+        {
+            this.UpVote = 0;
+            this.DownVote = 0;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int NewsId { get; set; }
+
+        [Required]
+        public int CreatedBy { get; set; }
+
+        public int UpVote { get; set; }
+
+        public int DownVote { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime UpdatedOn { get; set;}
+
+    }
+}
