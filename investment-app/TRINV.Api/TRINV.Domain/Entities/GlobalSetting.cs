a1b257dbafbc9a252c7c503f2dd645a752db18b2
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-22 20:27:25 +0200

    Added GlobalSetting Entity

---
+++ investment-app/TRINV.Api/TRINV.Domain/Entities/GlobalSetting.cs
@@ -0,0 +1,23 @@
+﻿namespace TRINV.Domain.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class GlobalSetting
+    {
+        public GlobalSetting()
+        {
+            IsManageable = false;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required] 
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        public bool IsManageable { get; set; }
+    }
+}
