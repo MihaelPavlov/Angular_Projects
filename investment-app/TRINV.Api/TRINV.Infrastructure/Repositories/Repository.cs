f3cd0fef187ef8f26173f6ccdf8a6ec44f81f494
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-24 19:24:08 +0200

    Removed namespace brackets and unnecessary empty lines.

--- investment-app/TRINV.Api/TRINV.Infrastructure/Repositories/Repository.cs
+++ investment-app/TRINV.Api/TRINV.Infrastructure/Repositories/Repository.cs
@@ -1,41 +1,40 @@
-﻿namespace TRINV.Infrastructure.Repositories
+﻿namespace TRINV.Infrastructure.Repositories;
+
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+public class Repository<T> : IRepository<T>
+    where T : class
 {
-    using Application.Interfaces;
-    using Microsoft.EntityFrameworkCore;
+    private readonly ApplicationDbContext _context;
 
-    public class Repository<T> : IRepository<T>
-        where T : class
+    public Repository(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public Repository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToArrayAsync();
+    }
 
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            return await _context.Set<T>().ToArrayAsync();
-        }
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
 
-        public async Task<T?> GetByIdAsync(int id)
-        {
-            return await _context.Set<T>().FindAsync(id);
-        }
+    public void Insert(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
 
-        public void Insert(T entity)
-        {
-            _context.Set<T>().Add(entity);
-        }
+    public void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
 
-        public void Update(T entity)
-        {
-            _context.Set<T>().Update(entity);
-        }
-
-        public void Delete(T entity)
-        {
-            _context.Set<T>().Remove(entity);
-        }
+    public void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
     }
 }
