e21087d486d39ec8de30dd1691711f22248b61e7
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-24 18:46:05 +0200

    Added generig IRepository interface in Application layer.

--- investment-app/TRINV.Api/TRINV.Application/Interfaces/IRepository.cs
+++ investment-app/TRINV.Api/TRINV.Application/Interfaces/IRepository.cs
@@ -1,5 +1,14 @@
 ﻿namespace TRINV.Application.Interfaces;
-    
-    public interface IRepository<T>
-    {
-    }
+
+public interface IRepository<T>
+{
+    Task<IEnumerable<T>> GetAllAsync();
+
+    Task<T?> GetByIdAsync(int id);
+
+    void Insert(T entity);
+
+    void Update(T entity);
+
+    void Delete(T entity);
+}
