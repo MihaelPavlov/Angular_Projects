78c26c7910b253420005b1e8217a2f2b41b4c2da
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-24 18:35:28 +0200

    Revert "Added generic IRepository interface in Application layer"
    This reverts commit 09806b48aa340bbbcd09666d985a29462e4ef898.

--- investment-app/TRINV.Api/TRINV.Application/Interfaces/IRepository.cs
+++ investment-app/TRINV.Api/TRINV.Application/Interfaces/IRepository.cs
@@ -1,14 +1,5 @@
 ﻿namespace TRINV.Application.Interfaces;
-
-public interface IRepository<T>
-{
-    Task<IEnumerable<T>> GetAllAsync();
-
-    Task<T?> GetByIdAsync(int id);
-
-    void Insert(T item);
-
-    void Update(T item);
-
-    void Delete(T item);
-}
+    
+    public interface IRepository<T>
+    {
+    }
