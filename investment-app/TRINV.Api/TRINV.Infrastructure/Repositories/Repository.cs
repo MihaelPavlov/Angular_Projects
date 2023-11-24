b8d4bc3c15d6037fbd313551093599159da83ca8
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-24 19:18:35 +0200

    Added generic repository calss in Infrastructure layer.

---
+++ investment-app/TRINV.Api/TRINV.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿namespace TRINV.Infrastructure.Repositories
+{
+    using Application.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToArrayAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
