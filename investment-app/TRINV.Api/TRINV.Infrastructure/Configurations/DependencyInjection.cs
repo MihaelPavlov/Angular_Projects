c7672f17c798e908e094264b76f0b37cce4cd4b9
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-24 20:27:29 +0200

    Added repository service in DependencyInjection

--- investment-app/TRINV.Api/TRINV.Infrastructure/Configurations/DependencyInjection.cs
+++ investment-app/TRINV.Api/TRINV.Infrastructure/Configurations/DependencyInjection.cs
@@ -1,14 +1,18 @@
 ﻿namespace TRINV.Infrastructure.Configurations;
 
+using Application.Interfaces;
 using Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Repositories;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IApplicationDbContextFactory, ApplicationDbContextFactory>();
+
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
         return services;
     }
