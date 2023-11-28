4a5b3e66377800aa8dd35a191d23739e561867b8
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-11-29 01:15:15 +0200

    Namespace fix

--- investment-app/TRINV.Api/TRINV.Infrastructure/Configurations/DI.cs
+++ investment-app/TRINV.Api/TRINV.Infrastructure/Configurations/DI.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TRINV.Application.Interfaces;
-using TRINV.Infrastructure.Repositories;
+using Repositories;
 
 public static class DI
 {
