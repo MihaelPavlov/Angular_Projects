af40c6b5a48de806c391d3d2509ebb87efa628c8
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-12-01 17:35:07 +0200

    Added UserController

---
+++ investment-app/TRINV.Api/TRINV.Application/Commands/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace TRINV.Application.Commands;
+
+using MediatR;
+
+internal record CreateUserCommand : IRequest<int>;
+
+internal class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
+{
+    public Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(1);
+    }
+}
---
+++ investment-app/TRINV.Api/TRINV.InvestTrackApplication/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿namespace TRINV.InvestTrackApplication.Controllers;
+
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
