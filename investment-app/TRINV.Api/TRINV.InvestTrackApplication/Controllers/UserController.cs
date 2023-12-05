0f942bb22ce5859a2095bab2f6894b16cef62dc1
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-12-05 18:01:23 +0200

    requestUri fixed

--- investment-app/TRINV.Api/TRINV.InvestTrackApplication/Controllers/UserController.cs
+++ investment-app/TRINV.Api/TRINV.InvestTrackApplication/Controllers/UserController.cs
@@ -25,7 +25,7 @@ public class UserController : ControllerBase
         var json = JsonSerializer.Serialize(command);
         var httpClient = new HttpClient();
         var content = new StringContent(json, Encoding.UTF8, "application/json");
-        var response = await httpClient.PostAsync("https://(localhost)/api/authenticate", content);
+        var response = await httpClient.PostAsync("https://localhost:5001/Account/CreateUser", content);
         if (response.IsSuccessStatusCode)
         {
             return Ok("New user added successfully.");
