20e6999322154a3c8ec88786fbcd0c27b8a52a21
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-12-11 09:31:14 +0200

    Added attributes to the command including email validation attribute.

--- investment-app/TRINV.IdentityServer/TRINV.IdentityServer/TRINV.IdentityServer/Commands/RegisterUserCommand.cs
+++ investment-app/TRINV.IdentityServer/TRINV.IdentityServer/TRINV.IdentityServer/Commands/RegisterUserCommand.cs
@@ -1,13 +1,18 @@
 ﻿namespace TRINV.IdentityServer.Commands;
 
+using System.ComponentModel.DataAnnotations;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using TRINV.IdentityServer.Data.Models;
 
 public class RegisterUserCommand : IRequest<IdentityResult>
 {
+    [Required]
     public string UserName { get; set; }
+
+    [EmailAddress]
     public string Email { get; set; }
+    [Required]
     public string Password { get; set; }
 }
 
