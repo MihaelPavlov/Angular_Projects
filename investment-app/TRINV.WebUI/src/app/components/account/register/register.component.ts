7478251fd61a834118a0c8ca3f9fbe80eea66673
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2023-12-12 19:22:50 +0200

    [IN-7.1] working on registration form

--- investment-app/TRINV.WebUI/src/app/components/account/register/register.component.ts
+++ investment-app/TRINV.WebUI/src/app/components/account/register/register.component.ts
@@ -1,17 +1,37 @@
 import {Component, OnInit} from "@angular/core";
 import {AuthService} from "../../../../shared/services/auth.service";
 import {ActivatedRoute} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: "register",
   templateUrl: 'register.component.html',
   styleUrls: ['register.component.css']
 })
-export class RegisterComponent implements OnInit{
-  constructor(  private activatedRoute: ActivatedRoute,private _authService: AuthService) {
+export class RegisterComponent implements OnInit {
+
+  registrationForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private activatedRoute: ActivatedRoute, private _authService: AuthService) {
   }
+
   ngOnInit() {
+    this.registrationForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.email],
+      password: [''],
+      confirmPassword: [''],
+      location: this.fb.group({
+        city: [''],
+        address: [''],
+        street: ['']
+      }),
+      notifications: this.fb.group({
+        // Add form controls for notifications as needed
+      })
+    })
   }
+
   public async login(): Promise<void> {
     await this._authService.loginStart()
   }
