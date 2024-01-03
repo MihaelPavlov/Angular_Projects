4db3663ef62e777b06ec4b7e129640a18b2d5855
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-03 14:30:55 +0200

    Added guard clause class and IsArgumentNull clause in domain layer.

---
+++ investment-app/TRINV.Api/TRINV.Domain/Validations/Ensure.cs
@@ -0,0 +1,16 @@
+﻿namespace TRINV.Domain.Validations;
+
+using System.Runtime.CompilerServices;
+
+public static class Ensure
+{
+    public static void IsArgumentNull(string? value,
+        [CallerArgumentExpression("value")] string? paramName = null)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException("The value can't be null", paramName);
+        }
+    }
+
+}
