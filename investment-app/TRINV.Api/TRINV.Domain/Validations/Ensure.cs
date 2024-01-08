e00f8dc7257001df8caf2051f41a52a948bee74b
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-08 14:50:14 +0200

    IsEqual simplified.

--- investment-app/TRINV.Api/TRINV.Domain/Validations/Ensure.cs
+++ investment-app/TRINV.Api/TRINV.Domain/Validations/Ensure.cs
@@ -34,21 +34,15 @@ public static class Ensure
 
     public static void IsEqual<T>(T? value1, T? value2)
     {
-        var value1Type = value1.GetType();
-        var value2Type = value2.GetType();
-        var propertyInfo1 = value1Type.GetProperties();
-        var propertyInfo2 = value2Type.GetProperties();
-        for (int i = 0; i < propertyInfo1.Length; i++)
+        Type type = typeof(T);
+        foreach (var field in type.GetFields())
         {
-            var propName1 = propertyInfo1[i].Name;
-            var propName2 = propertyInfo2[i].Name;
-            var propValue1 = propertyInfo1[i].GetValue(value1);
-            var propValue2 = propertyInfo2[i].GetValue(value2);
+            var fieldValue1 = field.GetValue(value1);
+            var fieldValue2 = field.GetValue(value2);
 
-            if (!propName1.Equals(propName2) || !propValue1.Equals(propValue2))
-            {
+            if (fieldValue1 == null && fieldValue2 == null) continue;
+            if (fieldValue1 == null || !fieldValue1.Equals(fieldValue2))
                 throw new ArgumentException("The objects are not equal");
-            }
         }
     }
 
