db49de2c9fe197cafb7aef775105078c9eb0470d
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-08 17:14:46 +0200

    Added GetNewsByIdAsync Query

---
+++ investment-app/TRINV.Api/TRINV.Application/Queries/News/GetNewsByIdQuery.cs
@@ -0,0 +1,29 @@
+﻿namespace TRINV.Application.Queries.News;
+
+using Domain.Entities;
+using Interfaces;
+using MediatR;
+using Shared.Business.Exceptions;
+
+public record GetNewsByIdQuery(int Id) : IRequest<News>;
+
+public class GetNewsByIdQueryHandler : IRequestHandler<GetNewsByIdQuery, News>
+{
+    readonly IRepository<News> _repository;
+
+    public GetNewsByIdQueryHandler(IRepository<News> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<News> Handle(GetNewsByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        if (result is null)
+        {
+            throw new NotFoundException(nameof(News), request.Id);
+        }
+
+        return result;
+    }
+}
