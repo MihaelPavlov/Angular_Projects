1ff48a725b6d5085005c6a7dcdfb16e44df0ec52
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-08 16:20:16 +0200

    Added DeleteNews command.

---
+++ investment-app/TRINV.Api/TRINV.Application/Commands/News/DeleteNewsCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace TRINV.Application.Commands.News;
+
+using Domain.Entities;
+using Interfaces;
+using MediatR;
+using Shared.Business.Exceptions;
+
+public record DeleteNewsCommand(int Id) : IRequest;
+
+public class DeleteNewsCommandHandler : IRequestHandler<DeleteNewsCommand>
+{
+    readonly IRepository<News> _repository;
+    readonly IUnitOfWork _unitOfWork;
+
+    public DeleteNewsCommandHandler(IRepository<News> repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(DeleteNewsCommand request, CancellationToken cancellationToken)
+    {
+        var newsToDelete = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        if (newsToDelete is null)
+        {
+            throw new NotFoundException(nameof(News), request.Id);
+        }
+
+        _repository.Delete(newsToDelete);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
