bbffb4c10939e3ce908f5cabbaf6ad8703ec7646
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-08 14:49:04 +0200

    IsEqual simplified.

---
+++ investment-app/TRINV.Api/TRINV.Application/Commands/News/CreateNewsCommand.cs
@@ -0,0 +1,40 @@
+﻿namespace TRINV.Application.Commands.News;
+
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+using static Domain.Validations.EntityValidationConstants.News;
+
+public record CreateNewsCommand : IRequest
+{
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(NameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MaxLength(DescriptionMaxLength)]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    public int ViewsCount { get; set; }
+
+    [Required]
+    public int UpVote { get; set; }
+
+    [Required]
+    public int DownVote { get; set; }
+
+    [Required]
+    [MaxLength(ImageUrlMaxLength)]
+    public string ImageUrl { get; set; } = null!;
+}
+
+public class CreateNewsCommandHandler : IRequestHandler<CreateNewsCommand>
+{
+    public Task Handle(CreateNewsCommand request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(Unit.Value);
+    }
+}
+
