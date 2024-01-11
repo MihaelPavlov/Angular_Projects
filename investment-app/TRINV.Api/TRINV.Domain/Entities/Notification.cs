fc9ad109e85adb5820ab0d4b0f6731530e796181
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-11 15:47:27 +0200

    Added Notification entity and NotificationType Enum.

---
+++ investment-app/TRINV.Api/TRINV.Domain/Entities/Notification.cs
@@ -0,0 +1,18 @@
+﻿namespace TRINV.Domain.Entities;
+
+using Enums;
+
+public class Notification : BaseEntity
+{
+    public int UserId { get; set; }
+
+    public NotificationType NotificationType { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool IsRead { get; set; } //IsSeen / IsRead / IsUnread 
+
+    public bool IsDeleted { get; set; }
+}
---
+++ investment-app/TRINV.Api/TRINV.Domain/Enums/NotificationType.cs
@@ -0,0 +1,8 @@
+﻿namespace TRINV.Domain.Enums;
+
+public enum NotificationType
+{
+    Email = 0,
+    Alert = 1,
+    System = 2
+}
