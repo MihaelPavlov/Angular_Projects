7eb41a798e7eb3e992046a6b8069ac3f98a3f86a
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-15 09:32:51 +0200

    GetNotificationByIdQuery Implemented.

---
+++ investment-app/TRINV.Api/TRINV.Application/Queries/Notification/GetNotificationByIdQuery.cs
@@ -0,0 +1,34 @@
+﻿namespace TRINV.Application.Queries.Notification;
+
+using Domain.Entities;
+using Interfaces;
+using MediatR;
+using Shared.Business.Exceptions;
+using Shared.Business.Utilities;
+
+public record GetNotificationByIdQuery(int Id) : IRequest<OperationResult<Notification>>;
+
+internal class
+    GetNotificationByIdQueryHandler : IRequestHandler<GetNotificationByIdQuery, OperationResult<Notification>>
+{
+    private readonly IRepository<Notification> _repository;
+
+    public GetNotificationByIdQueryHandler(IRepository<Notification> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<OperationResult<Notification>> Handle(GetNotificationByIdQuery request, CancellationToken cancellationToken)
+    {
+        var operationResult = new OperationResult<Notification>();
+
+        var notification = await _repository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (notification == null) 
+            return operationResult.ReturnWithErrorMessage(new NotFoundException("No notification was found!"));
+
+        operationResult.RelatedObject = notification;
+
+        return operationResult;
+    }
+}
