b2a0556b95f9048f682e46e42fbd37e5e605b1f9
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-15 09:07:13 +0200

    UpdateNotificationCommand Implemented.

--- investment-app/TRINV.Api/TRINV.Application/Commands/Notifications/UpdateNotificationCommand.cs
+++ investment-app/TRINV.Api/TRINV.Application/Commands/Notifications/UpdateNotificationCommand.cs
@@ -2,16 +2,19 @@
 
 using Domain.Entities;
 using Domain.Enums;
-using MediatR;
-using System.ComponentModel.DataAnnotations;
-using Shared.Business.Utilities;
-using Shared.Business.Exceptions;
-using Domain.Validations;
 using Interfaces;
+using MediatR;
+using Shared.Business.Exceptions;
+using Shared.Business.Utilities;
+using Domain.Validations;
+using System.ComponentModel.DataAnnotations;
 using static Domain.Validations.EntityValidationConstants.Notifications;
 
-public record CreateNotificationCommand : IRequest<OperationResult<Notification>>
+public record UpdateNotificationCommand : IRequest<OperationResult<Notification>>
 {
+    [Required]
+    public int Id { get; set; }
+
     [Required]
     public NotificationType NotificationType { get; set; }
 
@@ -20,19 +23,18 @@ public record CreateNotificationCommand : IRequest<OperationResult<Notification>
     public string Message { get; set; } = string.Empty;
 }
 
-internal class CreateNotificationCommandHandler : IRequestHandler<CreateNotificationCommand, OperationResult<Notification>>
+internal class UpdateNotificationCommandHandler : IRequestHandler<UpdateNotificationCommand, OperationResult<Notification>>
 {
     readonly IRepository<Notification> _repository;
     readonly IUnitOfWork _unitOfWork;
 
-    public CreateNotificationCommandHandler(IUnitOfWork unitOfWork, IRepository<Notification> repository)
+    public UpdateNotificationCommandHandler(IUnitOfWork unitOfWork, IRepository<Notification> repository)
     {
         _unitOfWork = unitOfWork;
         _repository = repository;
     }
 
-    public async Task<OperationResult<Notification>> Handle(CreateNotificationCommand request,
-        CancellationToken cancellationToken)
+    public async Task<OperationResult<Notification>> Handle(UpdateNotificationCommand request, CancellationToken cancellationToken)
     {
         var operationResult = new OperationResult<Notification>();
 
@@ -40,13 +42,16 @@ internal class CreateNotificationCommandHandler : IRequestHandler<CreateNotifica
             return operationResult.ReturnWithErrorMessage(
                 new NotFoundException($"{nameof(Notification)} of type: {request.NotificationType} was not found."));
 
-        var notification = new Notification
-        {
-            NotificationType = request.NotificationType,
-            Message = request.Message,
-        };
+        var notification = await _repository.GetByIdAsync(request.Id, cancellationToken);
 
-        await _repository.Insert(notification, cancellationToken);
+        if (notification == null)
+            return operationResult.ReturnWithErrorMessage(
+                new NotFoundException($"{nameof(Notification)} was not found."));
+
+        notification.NotificationType = request.NotificationType;
+        notification.Message = request.Message;
+
+        _repository.Update(notification);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
 
         operationResult.RelatedObject = notification;
