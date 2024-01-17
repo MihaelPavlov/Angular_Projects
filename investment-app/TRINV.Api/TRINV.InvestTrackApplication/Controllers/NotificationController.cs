c2071ab50527bb628c19810dbe9ae657c14cdb80
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-17 17:55:29 +0200

    Added NotificationController

---
+++ investment-app/TRINV.Api/TRINV.InvestTrackApplication/Controllers/NotificationController.cs
@@ -0,0 +1,11 @@
+﻿namespace TRINV.InvestTrackApplication.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        //TODO: implement Check the controller route Naming Convention
+    }
+}
