3323b7846d3990780b88b403207ba0de747e672b
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-18 13:18:04 +0200

    compare to method in EnomerationHelper implemented

--- investment-app/TRINV.Shared/TRINV.Shared.Business/Helpers/EnumerationHelper.cs
+++ investment-app/TRINV.Shared/TRINV.Shared.Business/Helpers/EnumerationHelper.cs
@@ -2,8 +2,6 @@
 
 public abstract class EnumerationHelper : IComparable
 {
-    //private static readonly Dictionary<int, TEnum> Enumerations = CreateEnumerations();
-
     protected EnumerationHelper(int id, string name)
     {
         Id = id;
@@ -13,30 +11,6 @@ public abstract class EnumerationHelper : IComparable
 
     public string Name { get; protected init; }
 
-    //public static TEnum FromValue(int value)
-    //{
-    //    return Enumerations.TryGetValue(
-    //        value,
-    //        out TEnum? enumeration)?
-    //            enumeration :
-    //            default!;
-    //}
-
-    //public static TEnum FromName(string name)
-    //{
-    //    return Enumerations
-    //        .Values
-    //        .SingleOrDefault(e => e.Name == name)!;
-    //}
-
-    //public bool Equals(Enumeration<TEnum>? other)
-    //{
-    //    if (other is null)
-    //        return false;
-
-    //    return GetType() == other.GetType() && 
-    //           Id.Equals(other.Id);
-    //}
 
     public override bool Equals(object? obj)
     {
@@ -47,25 +21,7 @@ public abstract class EnumerationHelper : IComparable
 
     public int CompareTo(object? obj)
     {
-        throw new NotImplementedException();
+        EnumerationHelper? other = obj as EnumerationHelper;
+        return this.Id.CompareTo(other.Id);
     }
-
-
-    //private static Dictionary<int, TEnum> CreateEnumerations()
-    //{
-    //    var enumerationTypes = typeof(TEnum);
-
-    //    var fieldsForType = enumerationTypes
-    //        .GetFields(
-    //            BindingFlags.Public |
-    //            BindingFlags.Static |
-    //            BindingFlags.FlattenHierarchy
-    //        )
-    //        .Where(fieldInfo =>
-    //            enumerationTypes.IsAssignableFrom(fieldInfo.FieldType))
-    //        .Select(fieldInfo =>
-    //            (TEnum)fieldInfo.GetValue(default)!);
-
-    //    return fieldsForType.ToDictionary(x => x.Id);
-    //}
 }
