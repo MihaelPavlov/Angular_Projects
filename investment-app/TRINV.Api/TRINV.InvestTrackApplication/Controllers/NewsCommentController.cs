b46d4e467cba4e9d2a9c4152e241f32eac2dd10e
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-01-23 01:27:01 +0200

    Created NewsCommentController with create news comment implementation.

---
+++ investment-app/TRINV.Api/TRINV.InvestTrackApplication/Controllers/NewsCommentController.cs
@@ -0,0 +1,25 @@
+﻿namespace TRINV.InvestTrackApplication.Controllers;
+
+using Application.Commands.NewsComment;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Business.Utilities;
+
+[Route("[controller]")]
+[ApiController]
+public class NewsCommentController : ControllerBase
+{
+    readonly IMediator _mediator;
+
+    public NewsCommentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OperationResult))]
+    public async Task<IActionResult> CreateNewsComment([FromBody] CreateNewsCommentCommand command, CancellationToken cancellationToken) =>
+        this.Ok(await _mediator.Send(command, cancellationToken));
+
+}
